3f99f05ab54efd8b70de31b7652218333b0d0fd6
Qais Patankar <qaisjp@gmail.com>
2017-10-07 21:41:48 +0100

    Tweak check error code

--- pkg/api/backend/check.go
+++ pkg/api/backend/check.go
@@ -7,12 +7,14 @@ import (
 )
 
 // Valid redirects the user to a thing to do a thing
+// - success: abuse checks pass, and cosign returns a code + message
+// - failure: cosign died somewhere, or the IP address is flagged for abuse
 func (i *Impl) Check(c *gin.Context) {
 	cookie := c.Param("login_cookie")
 	response, err := i.Filter.Check(cookie)
 
 	if err != nil {
-		c.JSON(http.StatusBadGateway, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"status":  "error",
 			"message": err.Error(),
 		})
